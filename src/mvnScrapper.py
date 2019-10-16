42208c18b35b7eb100f7958758ee69b2c9bd1489
Caio Shimada Rabello <xcaiosr@gmail.com>
2019-10-16 19:27:10 -0300

    adapted to test

--- src/mvnScrapper.py
+++ src/mvnScrapper.py
@@ -22,7 +22,7 @@ class MVNscrapper():
         headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
         req = requests.get(url, headers)
         soup = BeautifulSoup(req.content, 'html.parser', parse_only=SoupStrainer('a'))
-        sleep(2)
+        sleep(1)
 
         return soup
 
