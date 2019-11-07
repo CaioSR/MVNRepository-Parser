af04b7e1a4d08e417cb3559fb7183f8628dc8301
Caio Shimada Rabello <caio@MacBook-Pro-de-Antonio-Barros.local>
2019-11-07 18:06:26 -0300

    shiro-scrap

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from mvnScrapper import MVNscrapper
 import os
 
 #root = "https://mvnrepository.com/artifact/org.picketbox/picketbox/5.1.0.Final"
-root = "https://mvnrepository.com/artifact/org.springframework.security/spring-security-core/5.2.0.RELEASE"
+#root = "https://mvnrepository.com/artifact/org.springframework.security/spring-security-core/5.2.0.RELEASE"
+root = "https://mvnrepository.com/artifact/org.apache.shiro/shiro-core/1.4.1"
 max_depth = 4
 
 path = os.getcwd().replace('\\', '/') + "/test_files"
