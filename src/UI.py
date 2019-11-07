1914ba5b3e2a14118fc684181b82c2ef1da31b5d
Caio Shimada Rabello <caio@MacBook-Pro-de-Antonio-Barros.local>
2019-11-07 19:01:16 -0300

    fix loadOP

--- src/UI.py
+++ src/UI.py
@@ -17,16 +17,16 @@ class Interface:
     def __init__(self):
         self.home()
 
-    def home(self, ui = None):
+    def home(self):
         try:
-            ui.destroy()
+            self.ui.destroy()
         except:
             pass
 
-        ui = Tk()
-        ui.title("Deps Ahoy")
+        self.ui = Tk()
+        self.ui.title("Deps Ahoy")
 
-        description_frame = Frame(ui)
+        description_frame = Frame(self.ui)
         description_frame.grid(row=0, column=1)
 
         description_label = Label(description_frame, text="""      Deps Ahoy is a parser for online stored projects.
@@ -48,96 +48,96 @@ class Interface:
   finished, should you choose to do so.""", font='arial 10', justify=LEFT)
         description_label.pack(padx=5,pady=5)
 
-        buttons_frame = Frame(ui)
+        buttons_frame = Frame(self.ui)
         buttons_frame.grid(row=0, column=0, sticky=N)
-        new_op_button = Button(buttons_frame, anchor='nw', width=20, text="New operation", command=lambda : self.iniateNewOP(ui))
+        new_op_button = Button(buttons_frame, anchor='nw', width=20, text="New operation", command=lambda : self.iniateNewOP())
         new_op_button.grid(row=0, column=0, padx=5, pady=5)
-        load_op_button = Button(buttons_frame, anchor='nw', width=20, text="Load operation", command=lambda : self.loadOP(ui))
+        load_op_button = Button(buttons_frame, anchor='nw', width=20, text="Load operation", command=lambda : self.loadOP())
         load_op_button.grid(row=1, column=0, padx=5, pady=5)
         alter_file_button = Button(buttons_frame, anchor='nw', width=20, text="Alter files")
         alter_file_button.grid(row=2, column=0, padx=5, pady=5)
 
-        ui.mainloop()
+        self.ui.mainloop()
 
-    def iniateNewOP(self, ui):
-        ui.destroy()
-        ui = Tk()
-        ui.title("Deps Ahoy")
+    def iniateNewOP(self):
+        self.ui.destroy()
+        self.ui = Tk()
+        self.ui.title("Deps Ahoy")
 
         self.repository_input = StringVar()
         self.repository_input.set('MVNRepository')
         self.repo = self.repository_input.get()
 
-        repository_confirm_label = Label(ui, anchor='e', width=20, text="Repository: ")
+        repository_confirm_label = Label(self.ui, anchor='e', width=20, text="Repository: ")
         repository_confirm_label.grid(row=0, column=0, padx=5, pady=5)
-        repository_input_menu = OptionMenu(ui, self.repository_input, *self.available_repo)
+        repository_input_menu = OptionMenu(self.ui, self.repository_input, *self.available_repo)
         repository_input_menu.grid(row=0, column=1, sticky='w', padx=5, pady=5)
         self.repository_input.trace('w', self.setRepo) #observer
 
-        depth_confirm_label = Label(ui, anchor='e', width=20, text="Maximum depth search: ")
+        depth_confirm_label = Label(self.ui, anchor='e', width=20, text="Maximum depth search: ")
         depth_confirm_label.grid(row=1, column=0, padx=5, pady=5)
         depth_input = StringVar()
-        depth_input_box = Entry(ui, width=10, textvariable=depth_input)
+        depth_input_box = Entry(self.ui, width=10, textvariable=depth_input)
         depth_input_box.grid(row=1, column=1, sticky='w', padx=5, pady=5)
 
-        project_confirm_label = Label(ui, anchor='e', width=20, text="Project link with version: ")
+        project_confirm_label = Label(self.ui, anchor='e', width=20, text="Project link with version: ")
         project_confirm_label.grid(row=2, column=0, padx=5, pady=5)
         project_input = StringVar()
-        project_input_box = Entry(ui, width=50, textvariable=project_input)
+        project_input_box = Entry(self.ui, width=50, textvariable=project_input)
         project_input_box.grid(row=2, column=1, sticky='w', padx=5, pady=5)
 
-        progress_file_dir = Label(ui, anchor='e', width=20, text="Progress files directory: ")
+        progress_file_dir = Label(self.ui, anchor='e', width=20, text="Progress files directory: ")
         progress_file_dir.grid(row=3, column=0, padx=5, pady=5)
         progress_dir_input = StringVar(value = os.getcwd().replace('\\', '/') + '/prog-manag/')
-        progress_dir_input = Entry(ui, width=50, textvariable=progress_dir_input)
+        progress_dir_input = Entry(self.ui, width=50, textvariable=progress_dir_input)
         progress_dir_input.grid(row=3, column=1, sticky='w', padx=5, pady=5)
 
-        final_file_dir = Label(ui, anchor='e', width=20, text="Progress files directory: ")
+        final_file_dir = Label(self.ui, anchor='e', width=20, text="Progress files directory: ")
         final_file_dir.grid(row=4, column=0, padx=5, pady=5)
         final_dir_input = StringVar(value = os.getcwd().replace('\\', '/') + '/final/')
-        final_dir_input = Entry(ui, width=50, textvariable=final_dir_input)
+        final_dir_input = Entry(self.ui, width=50, textvariable=final_dir_input)
         final_dir_input.grid(row=4, column=1, sticky='w', padx=5, pady=5)
 
-        previous_screen_button = Button(ui, width=20, text="Return to home", command=lambda : self.home(ui))
+        previous_screen_button = Button(self.ui, width=20, text="Return to home", command=lambda : self.home())
         previous_screen_button.grid(row=5, column=0, sticky='w', padx=5, pady=5)
 
-        confirm_button = Button(ui, width=20, text="Begin Parse", command=lambda : self.finalize(ui, project_input, depth_input, progress_dir_input, final_dir_input))
+        confirm_button = Button(self.ui, width=20, text="Begin Parse", command=lambda : self.finalize(project_input.get(), int(depth_input.get()), progress_dir_input.get(), final_dir_input.get()))
         confirm_button.grid(row=5, column=1, sticky='e', padx=5, pady=5)
 
-        ui.mainloop()
+        self.ui.mainloop()
 
-    def loadOP(self, ui):
+    def loadOP(self):
         opDirectory = filedialog.askdirectory(initialdir = "./", title = "Select desired operation progress management folder")
         #askopenfilename(filetypes=[("python files","*.py"),("all files", "*.*")])
         folders = opDirectory.split('/')
         config = configparser.ConfigParser()
         config.read(opDirectory+'/config.ini')
-        self.repo = config.get('Operation Atributes', 'repository')
-        self.project_link = config.get('Operation Atributes', 'project link')
-        self.max_depth = int(config.get('Operation Atributes', 'maximum depth'))
-        self.final_dir = config.get('Operation Atributes', 'end directory')
-        self.progress_dir = folders[:-1]
-        if self.repo == 'MVNRepository':
-            scrapper = MVNscrapper(self.project_link, self.max_depth, self.final_dir, self.progress_dir)
+        repo = config.get('Operation Atributes', 'repository')
+        project_url = config.get('Operation Atributes', 'project link')
+        max_depth = int(config.get('Operation Atributes', 'maximum depth'))
+        final_dir = config.get('Operation Atributes', 'end directory')
+        progress_dir = '/'.join(folders[:-1])
         
-        self.initiateOP(scrapper)
+        self.finalize(project_url, max_depth, progress_dir, final_dir, repo = repo)
 
     def setRepo(self, *args):
         self.repo = self.repository_input.get()
 
-    def setAttributes(self, project, depth, progress, final):
-        self.project_link = project.get()
-        self.max_depth = int(depth.get())
-        self.progress_dir = progress.get()
-        self.final_dir = final.get()
+    def setAttributes(self, project, depth, progress, final, repo = None):
+        if repo:
+            self.repo = repo
+        self.project_link = project
+        self.max_depth = depth
+        self.progress_dir = progress
+        self.final_dir = final
 
     def initiateOP(self, scrapper):
         scrapper.scrapper()
 
-    def finalize(self, ui, project, depth, prog_dir, final_dir):
-        self.setAttributes(project, depth, prog_dir, final_dir)
-        ui.destroy()
+    def finalize(self, project, depth, prog_dir, final_dir, repo = None):
+        self.setAttributes(project, depth, prog_dir, final_dir, repo)
         self.printAll()
+        self.ui.destroy()
         if self.repo == 'MVNRepository':
             scrapper = MVNscrapper(self.project_link, self.max_depth, self.final_dir, self.progress_dir)
         self.initiateOP(scrapper)
